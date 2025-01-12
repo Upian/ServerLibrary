cafe4ac3cefa2613b5929a5989ed7b44bbf669ff
Upian <terry0729@naver.com>
2025-01-12 22:16:18 +0900

    소켓 작업
    AcceptEX, Connect, ConnectEX

--- Server/TCP_Server/IOCPSocket.cpp
+++ Server/TCP_Server/IOCPSocket.cpp
@@ -1,13 +1,14 @@
 #include "IOCPSocket.h"
+#include <Ws2tcpip.h>
 
-bool IOCPSocket::CerateSocket(int _adressFamily, int _socketType, IPPROTO _protocol, LPWSAPROTOCOL_INFO _ipProtocolInfo, int _group, int _wsaFlag)
+bool IOCPSocket::CerateSocket(int _adressFamily, int _socketType, IPPROTO _protocol, LPWSAPROTOCOL_INFO _ipProtocolInfo, GROUP _group, DWORD _wsaFlag)
 {
 	m_socket = WSASocket(_adressFamily, _socketType, _protocol, _ipProtocolInfo, _group, _wsaFlag);
 //	WSASocket(AF_INET, SOCK_STREAM, IPPROTO_TCP, NULL, 0, WSA_FLAG_OVERLAPPED);
 	return INVALID_SOCKET != m_socket;
 }
 
-bool IOCPSocket::BindSocket(const char* _ip, unsigned short _port)
+bool IOCPSocket::Bind(const char* _ip, unsigned short _port)
 {
 	SOCKADDR_IN addr;
 
@@ -16,7 +17,8 @@ bool IOCPSocket::BindSocket(const char* _ip, unsigned short _port)
 	if (nullptr == _ip)
 		addr.sin_addr.s_addr = ::htonl(INADDR_ANY);
 	else
-		addr.sin_addr.s_addr = inet_addr(_ip);
+		inet_pton(AF_INET, _ip, &(addr.sin_addr)); //addr.sin_addr.s_addr = inet_addr(_ip);
+
 
 	return 0 == bind(m_socket, (sockaddr*)&addr, sizeof(addr));
 }
@@ -26,3 +28,70 @@ bool IOCPSocket::Listen(int backlog /*= SOMAXCONN*/)
 	return 0 == listen(m_socket, backlog);
 }
 
+SOCKET IOCPSocket::Accept(sockaddr* addr, int* addrlen)
+{
+	return ::accept(m_socket, addr, addrlen);
+}
+
+bool IOCPSocket::AcceptEX(IOCPSocket* _listenSocket, PVOID lpOutputBuffer, DWORD dwReceiveDataLength, DWORD dwLocalAddressLength, DWORD dwRemoteAddressLength, LPDWORD lpdwBytesReceived, LPOVERLAPPED lpOverlapped)
+{
+	if (FALSE == AcceptEx(_listenSocket->GetSocket(), m_socket/*client socket*/, lpOutputBuffer, dwReceiveDataLength, dwLocalAddressLength,
+		dwRemoteAddressLength, lpdwBytesReceived, lpOverlapped))
+	{
+		int err = WSAGetLastError();
+		if (WSA_IO_PENDING != err && WSAEWOULDBLOCK != err)
+			return false;
+	}
+	return true;
+}
+
+bool IOCPSocket::Connect(const char* _ip, USHORT _port)
+{
+	SOCKADDR_IN ServerAddress;
+	ServerAddress.sin_family = AF_INET;
+	ServerAddress.sin_port = ::htons(_port);
+	inet_pton(AF_INET, _ip, &(ServerAddress.sin_addr)); //ServerAddress.sin_addr.s_addr = inet_addr(_ip);
+
+	if (SOCKET_ERROR == connect(m_socket, (SOCKADDR*)&ServerAddress, sizeof(ServerAddress)))
+	{
+		int err = ::WSAGetLastError();
+		if (WSA_IO_PENDING != err && WSAEWOULDBLOCK != err)
+			return false;
+	}
+	return true;
+}
+
+bool IOCPSocket::ConnectEx(const char* _ip, USHORT _port, PVOID _lpSendBuffer, DWORD _dwSendDataLength, LPDWORD _lpdwBytesSent, LPOVERLAPPED _lpOverlapped)
+{
+	SOCKADDR_IN ServerAddress;
+	ServerAddress.sin_family = AF_INET;
+	ServerAddress.sin_port = ::htons(_port);
+	inet_pton(AF_INET, _ip, &(ServerAddress.sin_addr)); //ServerAddress.sin_addr.s_addr = inet_addr(_ip);
+
+//	return this->ConnectEx(&ServerAddress, _lpSendBuffer, _dwSendDataLength, _lpdwBytesSent, _lpOverlapped);
+
+	if (m_lpfnConnectEX)
+	{
+		if (FALSE == m_lpfnConnectEX(m_socket, (LPSOCKADDR)&ServerAddress, sizeof(SOCKADDR_IN), _lpSendBuffer, _dwSendDataLength, _lpdwBytesSent, _lpOverlapped))
+		{
+			int err = ::WSAGetLastError();
+			if (WSA_IO_PENDING != err && WSAEWOULDBLOCK != err)
+				return false;
+		}
+	}
+	return true;
+}
+
+bool IOCPSocket::ConnectEx(SOCKADDR_IN* _addr, PVOID _lpSendBuffer, DWORD _dwSendDataLength, LPDWORD _lpdwBytesSent, LPOVERLAPPED _lpOverlapped)
+{
+	if (m_lpfnConnectEX)
+	{
+		if (FALSE == m_lpfnConnectEX(m_socket, (LPSOCKADDR)_addr, sizeof(SOCKADDR_IN), _lpSendBuffer, _dwSendDataLength, _lpdwBytesSent, _lpOverlapped))
+		{
+			int err = ::WSAGetLastError();
+			if (WSA_IO_PENDING != err && WSAEWOULDBLOCK != err)
+				return false;
+		}
+	}
+	return true;
+}
--- Server/TCP_Server/IOCPSocket.h
+++ Server/TCP_Server/IOCPSocket.h
@@ -1,15 +1,27 @@
 #pragma once
 #include <winsock2.h>
+#include <MSWSock.h>
+/*
+* 소켓 구조체
+*/
 
 class IOCPSocket
 {
 public:
-	bool CerateSocket(int _adressFamily, int _socketType, IPPROTO _protocol, LPWSAPROTOCOL_INFO _ipProtocolInfo, int _group, int _wsaFlag);
-	bool BindSocket(const char* _ip, unsigned short _port);
-	bool Listen(int backlog = SOMAXCONN);
+	SOCKET GetSocket() { return m_socket; }
 
+	bool CerateSocket(int _adressFamily, int _socketType, IPPROTO _protocol, LPWSAPROTOCOL_INFO _ipProtocolInfo, GROUP _group, DWORD _wsaFlag);
+	bool Bind(const char* _ip, USHORT _port);
+	bool Listen(int backlog = SOMAXCONN);
+	SOCKET Accept(sockaddr* _addr, int* _addrlen);
+	bool AcceptEX(IOCPSocket* _listenSocket, PVOID lpOutputBuffer, DWORD dwReceiveDataLength, DWORD dwLocalAddressLength, DWORD dwRemoteAddressLength, LPDWORD lpdwBytesReceived, LPOVERLAPPED lpOverlapped);
+	bool Connect(const char* _ip, USHORT _port);
+	bool ConnectEx(const char* _ip, USHORT _port, PVOID _lpSendBuffer, DWORD _dwSendDataLength, LPDWORD _lpdwBytesSent, LPOVERLAPPED _lpOverlapped);
+	bool ConnectEx(SOCKADDR_IN* _addr, PVOID _lpSendBuffer, DWORD _dwSendDataLength, LPDWORD _lpdwBytesSent, LPOVERLAPPED _lpOverlapped);
+	//recv
+	//send
 private:
 	SOCKET m_socket = INVALID_SOCKET;
-
+	LPFN_CONNECTEX m_lpfnConnectEX;
 
 };
