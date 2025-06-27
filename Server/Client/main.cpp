c223c3fbaddbf35d429f6a002663136c5d67caae
Upian <terry0729@naver.com>
2025-06-27 11:28:26 +0900

    Update TestClient

--- Server/Client/main.cpp
+++ Server/Client/main.cpp
@@ -1,85 +1,159 @@
 #define _WINSOCK_DEPRECATED_NO_WARNINGS
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
 #include <winsock2.h>
+#include <mswsock.h>
 #include <ws2tcpip.h>
+#include <windows.h>
 #include <iostream>
-#include <thread>
 #include <vector>
+#include <thread>
+#include <atomic>
 #include <chrono>
 #include <format>
 
 #pragma comment(lib, "ws2_32.lib")
+#pragma comment(lib, "mswsock.lib")
 
-void ClientThread(int _index, std::string _ip, short _port)
+LPFN_CONNECTEX GetConnectEx(SOCKET sock)
 {
-	SOCKET sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	if (sock == INVALID_SOCKET)
-	{
-		std::cerr << "[" << _index << "] Socket creation failed.\n";
-		return;
-	}
-
-	sockaddr_in serverAddr{};
-	serverAddr.sin_family = AF_INET;
-	serverAddr.sin_port = htons(_port);
-	serverAddr.sin_addr.s_addr = inet_addr(_ip.c_str());
-
-	if (connect(sock, (sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
-	{
-		std::cerr << "[" << _index << "] Connection failed.\n";
-		closesocket(sock);
-		return;
-	}
-
-	std::cout << "[" << _index << "] Connected to server.\n";
-	
-	// 메시지 보내기
-	std::string msg = "Hello from client " + std::to_string(_index);
-	send(sock, msg.c_str(), (int)msg.size(), 0);
-
-	// 응답 받기 (선택)
-	char buffer[512]{};
-	int recvLen = recv(sock, buffer, sizeof(buffer) - 1, 0);
-	if (recvLen > 0)
-	{
-		buffer[recvLen] = '\0';
-		std::cout << "[" << _index << "] Received: " << buffer << "\n";
-	}
-
-	std::this_thread::sleep_for(std::chrono::milliseconds(100)); // 테스트용 대기
-	closesocket(sock);
+	GUID guid = WSAID_CONNECTEX;
+	LPFN_CONNECTEX fn = nullptr;
+	DWORD bytes = 0;
+	WSAIoctl(sock, SIO_GET_EXTENSION_FUNCTION_POINTER,
+		&guid, sizeof(guid), &fn, sizeof(fn), &bytes, nullptr, nullptr);
+	return fn;
 }
+
+struct IOData : public OVERLAPPED
+{
+	WSABUF wsaBuf;
+	char buffer[1024];
+	enum class IOType { Connect, Send, Recv } type;
+};
+
+class ClientWorker
+{
+public:
+	ClientWorker(const char* ip, int port)
+		: m_ip(ip), m_port(port)
+	{
+	}
+
+	void Run(int _i)
+	{
+		SOCKET sock = WSASocket(AF_INET, SOCK_STREAM, 0, nullptr, 0, WSA_FLAG_OVERLAPPED);
+		if (sock == INVALID_SOCKET) return;
+
+		sockaddr_in local = {};
+		local.sin_family = AF_INET;
+		local.sin_addr.s_addr = INADDR_ANY;
+		bind(sock, (sockaddr*)&local, sizeof(local));
+
+		LPFN_CONNECTEX ConnectEx = GetConnectEx(sock);
+		if (!ConnectEx) return;
+
+		sockaddr_in server = {};
+		server.sin_family = AF_INET;
+		inet_pton(AF_INET, m_ip, &server.sin_addr);
+		server.sin_port = htons(m_port);
+
+		HANDLE iocp = CreateIoCompletionPort((HANDLE)sock, NULL, 0, 0);
+
+		IOData* io = new IOData{};
+		io->type = IOData::IOType::Connect;
+
+		DWORD sent = 0;
+		BOOL ret = ConnectEx(sock, (sockaddr*)&server, sizeof(server),
+			nullptr, 0, &sent, io);
+
+		if (!ret && WSAGetLastError() != WSA_IO_PENDING)
+		{
+			std::cerr << "ConnectEx error\n";
+			delete io;
+			closesocket(sock);
+			return;
+		}
+
+		OVERLAPPED* ov = nullptr;
+		ULONG_PTR key = 0;
+		DWORD transferred = 0;
+		size_t num = 0;
+		while (true)
+		{
+			BOOL ok = GetQueuedCompletionStatus(iocp, &transferred, &key, &ov, INFINITE);
+			if (!ok || !ov) break;
+
+			IOData* data = reinterpret_cast<IOData*>(ov);
+
+			if (data->type == IOData::IOType::Connect)
+			{
+				setsockopt(sock, SOL_SOCKET, SO_UPDATE_CONNECT_CONTEXT, nullptr, 0);
+				std::cout << "[Connected]\n";
+
+				memset(data->buffer, 0, sizeof(data->buffer));
+				std::string msg = std::format("Hello from client {0}, {1}", _i, num++);
+				strcpy_s(data->buffer, msg.c_str());
+				data->wsaBuf.buf = data->buffer;
+				data->wsaBuf.len = strlen(data->buffer);
+				data->type = IOData::IOType::Send;
+
+				WSASend(sock, &data->wsaBuf, 1, nullptr, 0, data, nullptr);
+			}
+			else if (data->type == IOData::IOType::Send)
+			{
+				std::cout << "[Sent] Waiting for echo...\n";
+				memset(data->buffer, 0, sizeof(data->buffer));
+				data->wsaBuf.buf = data->buffer;
+				data->wsaBuf.len = sizeof(data->buffer);
+				data->type = IOData::IOType::Recv;
+				DWORD flags = 0;
+				WSARecv(sock, &data->wsaBuf, 1, nullptr, &flags, data, nullptr);
+			}
+			else if (data->type == IOData::IOType::Recv)
+			{
+				std::cout << "[Recv] " << data->buffer << "\n";
+				
+				memset(data->buffer, 0, sizeof(data->buffer));
+				std::string msg = std::format("Hello from client {0}, {1}", _i, num++);
+				strcpy_s(data->buffer, msg.c_str());
+				data->wsaBuf.buf = data->buffer;
+				data->wsaBuf.len = strlen(data->buffer);
+				data->type = IOData::IOType::Send;
+
+				WSASend(sock, &data->wsaBuf, 1, nullptr, 0, data, nullptr);
+
+				break;
+			}
+		}
+
+		closesocket(sock);
+		CloseHandle(iocp);
+	}
+
+private:
+	const char* m_ip;
+	int m_port;
+};
+
 
 int main()
 {
-	WSADATA wsaData{};
-	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+	WSAData wsaData;
+	WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+	constexpr int CLIENT_COUNT = 10;
+	std::vector<std::jthread> threads;
+
+	for (int i = 0; i < CLIENT_COUNT; ++i)
 	{
-		std::cerr << "WSAStartup failed.\n";
-		return 1;
+		threads.emplace_back([&i] {
+			ClientWorker client("127.0.0.1", 8888);
+			client.Run(i);
+			});
+		std::this_thread::sleep_for(std::chrono::milliseconds(10)); // 서버 부담 줄이기
 	}
 
-	std::string ip = "127.0.0.1";
-	short port = 8888;
-	int clientCount = 0;
-
-//	std::cout << "input IP: ";
-//	std::cin >> ip;
-//	std::cout << "input port: ";
-//	std::cin >> port;
-	std::cout << "input client count: ";
-	std::cin >> clientCount;
-	
-	std::string str = std::format("IP: {0}, port: {1}, ClientCount: {2}", ip, port, clientCount);
-	std::cout << str << std::endl;
-
-	std::vector<std::jthread> clientThreads;
-	for (int i = 0; i < clientCount; ++i)
-	{
-		clientThreads.emplace_back(ClientThread, i, ip, port);
-//		std::this_thread::sleep_for(std::chrono::milliseconds(10)); // 약간의 간격
-	}
-
-	WSACleanup();
 	getchar();
+	WSACleanup();
 	return 0;
 }
